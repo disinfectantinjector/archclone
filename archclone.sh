ac1447f94257b2a09af36454427b1688bd303b10
Disinfectant Injector <disinfectantinjector@gmail.com>
2022-04-11 05:54:22 +0000

    Improve package matching

--- archclone.sh
+++ archclone.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-repo=$(pacman --color=never -Ss ^"$1"$ | grep "$1" | cut -d'/' -f1)
+repo=$(pacman --color=never -Ss ^"$1"$ | grep ".*/$1 " | cut -d'/' -f1)
 
 case $repo in
     core | extra) repo="packages";;
     community) ;;
-    *) echo Error: "$1" not found && exit 1 ;;
+    *) echo Error: Exact match "$1" not found && exit 1 ;;
 esac
 
 git clone --branch packages/"$1" --single-branch https://github.com/archlinux/svntogit-"$repo"
